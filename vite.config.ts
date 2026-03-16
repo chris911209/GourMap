0f881d495c02c9ab81d6e1811e63b0eb11a3ebea
Dogeon188 <dogcatdog188@gmail.com>
2026-03-17 00:14:23 +0800

    feat: initialize project w/ vite + svelte + paraglide

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        paraglideVitePlugin({
+            project: "./project.inlang",
+            outdir: "./src/paraglide",
+        }),
+        svelte(),
+    ],
+});
